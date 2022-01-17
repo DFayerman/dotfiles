5a0eea02233e43819fc3f36a91794ca6f12dcde4
DFayerman <davidfayerman@gmail.com>
2022-01-17 11:09:32 -0800

    convenience aliases and commenting

--- keybindings.zsh
+++ keybindings.zsh
@@ -1,16 +1,18 @@
 # aliases
 
+# general
 alias mkdir="mkdir -pv";
 alias c="clear";
 alias wget="wget -c";
 alias whatisthis="cat /etc/os-release"
-# alias update="sudo apt update && sudo -S apt -y upgrade"
 alias l="ls -sh1FAur --group-directories-first --color=auto"
+alias excel="/mnt/c/Program\ Files/Microsoft\ Office/root/Office16/EXCEL.EXE"
+# navigation
+alias ..="cd .."
+alias ...="cd .. & cd .."
 alias ~="cd ~"
 alias cdc="cd ~/code"
 alias cdo="cd /mnt/c/Users/david/OneDrive/content"
-alias tmux="tmux -f ~/dotfiles/tmux.conf"
-alias excel="/mnt/c/Program\ Files/Microsoft\ Office/root/Office16/EXCEL.EXE"
 # Python
 alias py="python"
 alias venv="virtualenv .venv"
@@ -19,6 +21,8 @@ alias activate='source .venv/bin/activate'
 # Git
 alias ga="git add -A"
 alias gcm="git commit -m"
+# tmux
+alias tmux="tmux -f ~/dotfiles/tmux.conf"
 
 ## Neovim aliases for sanity
 
@@ -41,16 +45,16 @@ alias nvi="nvim"
 update() {
 	sudo apt update
 	sudo apt -y upgrade
-	echo -n ".\n.\n.\n.\nUpdate NPM global packages (y/n)? "
+	echo -n ".\n.\n.\n.\nUpdate NPM global packages (y/n)?"
 	read answer
 	if [ "$answer" != "${answer#[Yy]}" ] ;then 
 			npm -g update
 	else
-			echo 'go nuts..'
+			echo "go nuts.."
 	fi
 }
 
-# may no longer work
+# MAY NO LONGER WORK
 updateGoLatest() {
 	cd /
 	GOVER="$(curl -s https://golang.org/dl/|grep -Eom1 '/dl.*gz')"
@@ -62,6 +66,7 @@ updateGoLatest() {
 	echo "updated to $(go version)"
 }
 
+# for fuzzy finder keybind
 fzf_find_and_open() {
 	file="$(fzf)"
 	cd $HOME/${file%/*}
